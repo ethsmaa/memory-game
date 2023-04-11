0b0cd8d0f15f7dbb8400f972a83125cb91966902
esma <esmaoruccs@gmail.com>
2023-04-11 22:13:27 +0300

    SLL class eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Random;
@@ -10,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        LinkedList<String> AnimalSLL = new LinkedList<String>(); // all animals in file
+        SLL AnimalSLL = new SLL(); // all animals in file
 
 
         // read animals from file and add to AnimalsSLL
@@ -33,17 +32,21 @@ public class Main {
         Random rnd = new Random();
 
 
-        System.out.println("Please enter n");
+        System.out.print("Please enter n ");
         int n = input.nextInt();
+        while (n> AnimalSLL.size()) {
+            System.out.print("please enter a smaller value.");
+            n = input.nextInt();
+        }
 
-        LinkedList<String> SLL1 = selectAnimals(AnimalSLL, n); // sll1
-        LinkedList<String> SLL2 = createSLL2(SLL1); // sll2
+        SLL SLL1 = selectAnimals(AnimalSLL, n); // sll1
+        SLL SLL2 = createSLL2(SLL1); // sll2
 
-        LinkedList<String> SLL3 = new LinkedList<String>(); // sll3 (names)
-        LinkedList<Integer> SLL4 = new LinkedList<Integer>(); // sll4 (scores)
+        SLL SLL3 = new SLL(); // sll3 (names)
+        SLL SLL4 = new SLL(); // sll4 (scores)
 
-        System.out.println(String.format("SLL1: %-60s  \t Score: %d", printAnimals(SLL1), 0));
-        System.out.println(String.format("SLL2: %-60s", printAnimals(SLL2)));
+        System.out.println(String.format("SLL1: %-70s  \t Score: %d", printAnimals(SLL1), 0));
+        System.out.println(String.format("SLL2: %-70s", printAnimals(SLL2)));
 
         int score = 0;
 
@@ -56,7 +59,7 @@ public class Main {
             int random2 = rnd.nextInt(SLL2.size()); // index of random animal in SLL2
 
             // while printing, I am adding 1. Because index starts from 0.
-            System.out.println(String.format("Randomly generated numbers %d  %d  %44s: %d ", (random1 + 1), (random2 + 1), "Step", stepCount));
+            System.out.println(String.format("Randomly generated numbers %d  %d  %52s: %d ", (random1 + 1), (random2 + 1), "Step", stepCount));
             System.out.println();
 
             if (checkForIdenticalAnimals(SLL1, SLL2, random1, random2)) { // if animals are identical
@@ -67,8 +70,8 @@ public class Main {
                 score -= 1; // player loses 1 point
             }
 
-            System.out.println(String.format("SLL1: %-60s  \t Score: %d", printAnimals(SLL1), score));
-            System.out.println(String.format("SLL2: %-60s", printAnimals(SLL2)));
+            System.out.println(String.format("SLL1: %-70s  \t Score: %d", printAnimals(SLL1), score));
+            System.out.println(String.format("SLL2: %-70s", printAnimals(SLL2)));
             System.out.println();
 
 
@@ -94,24 +97,24 @@ public class Main {
     }
 
     // random animal selection
-    static LinkedList selectAnimals(LinkedList<String> AnimalSLL, int n) {
-        LinkedList<String> SLL1 = new LinkedList<String>();
+    static SLL selectAnimals(SLL AnimalSLL, int n) {
+        SLL SLL1 = new SLL();
         Random rnd = new Random();
 
         for (int i = 0; i < n; i++) {
             int index = rnd.nextInt(AnimalSLL.size()); // select random index from AnimalSLL
-            String animal = AnimalSLL.get(index);
+            String animal = (String) AnimalSLL.get(index);
             SLL1.add(animal); // add animal to SLL1
-            AnimalSLL.remove(animal); // remove selected animal from AnimalSLL
+            AnimalSLL.remove(index); // remove selected animal from AnimalSLL
         }
 
         return SLL1;
     }
 
 
-    static LinkedList createSLL2(LinkedList<String> SLL1) {
-        LinkedList<String> tempSLL1 = new LinkedList<String>();
-        LinkedList<String> SLL2 = new LinkedList<String>();
+    static SLL createSLL2(SLL SLL1) {
+        SLL tempSLL1 = new SLL();
+        SLL SLL2 = new SLL();
 
         // copy SLL1 to tempSLL1
         for (int i = 0; i < SLL1.size(); i++) {
@@ -123,9 +126,9 @@ public class Main {
         int size = tempSLL1.size();
         while (size > 0) {
             int index = rnd.nextInt(tempSLL1.size());
-            String animal = tempSLL1.get(index);
+            String animal = (String) tempSLL1.get(index);
             SLL2.add(animal);
-            tempSLL1.remove(animal);
+            tempSLL1.remove(index);
             size--;
         }
 
@@ -133,7 +136,7 @@ public class Main {
     }
 
     // print lists
-    static String printAnimals(LinkedList<String> SLL) {
+    static String printAnimals(SLL SLL) {
         String animals = "";
         for (int i = 0; i < SLL.size(); i++) {
             animals += SLL.get(i) + "   ";
@@ -142,14 +145,14 @@ public class Main {
     }
 
     // check if selected animals are identical
-    static boolean checkForIdenticalAnimals(LinkedList<String> SLL1, LinkedList<String> SLL2, int random1, int random2) {
+    static boolean checkForIdenticalAnimals(SLL SLL1, SLL SLL2, int random1, int random2) {
         if (SLL1.get(random1).equals(SLL2.get(random2)))
             return true;
         else
             return false;
     }
 
-    static void readHighScore(LinkedList<String> SLL3, LinkedList<Integer> SLL4) {
+    static void readHighScore(SLL SLL3, SLL SLL4) {
 
         try {
             File file = new File("highscoretable.txt");
@@ -170,17 +173,17 @@ public class Main {
 
     }
 
-    static void sortList(LinkedList<String> SLL3, LinkedList<Integer> SLL4) {
+    static void sortList(SLL SLL3, SLL SLL4) {
 
         // sort SLL4 largest to smallest
         for (int i = 0; i < SLL4.size(); i++) {
             for (int j = 0; j < SLL4.size(); j++) {
-                if (SLL4.get(i) > SLL4.get(j)) {
-                    int temp = SLL4.get(i); // swap
+                if ((int) SLL4.get(i) > (int)SLL4.get(j)) {
+                    int temp = (int)SLL4.get(i); // swap
                     SLL4.set(i, SLL4.get(j)); // change positions
                     SLL4.set(j, temp);
 
-                    String temp2 = SLL3.get(i);
+                    String temp2 = (String) SLL3.get(i);
                     SLL3.set(i, SLL3.get(j));
                     SLL3.set(j, temp2);
                 }
@@ -202,7 +205,7 @@ public class Main {
 
     }
 
-    static void highScoreTable(LinkedList<String> SLL3, LinkedList<Integer> SLL4) {
+    static void highScoreTable(SLL SLL3, SLL SLL4) {
 
         int count = 0;
 
